49c7bdfaed17140fb570a2e2ab1b60b12b0d3e24
Samuel Tazzyman <samuel.tazzyman@justice.gsi.gov.uk>
2018-04-26 14:09:01 +0000

    Initial R work; packrat

---
+++ LSA.R
@@ -0,0 +1,56 @@
+library(tm)
+library(slam)
+library(tibble)
+
+#Helper functions
+
+#This normalises the lengths of a matrix to length 1
+normalize <- function(mat){
+  col.lengths <- sapply(1:ncol(mat), function(x) sqrt(sum(mat[, x]^2)))
+  return(sweep(mat, 2, col.lengths, "/"))
+}
+
+#This gives the positions of documents under a rank-k approximation
+#further projected onto a rank k-1 unit hypersphere.
+#The positions are given relevant to the original vector space.
+posns_k <- function(TDM, k){
+  svdecomp <- svd(TDM)
+  posns <- svdecomp$u[,1:k] %*% diag(svdecomp$d[1:k]) %*% t(svdecomp$v[,1:k]) %>%
+    normalize()
+  dimnames(posns) <- dimnames(TDM)
+  return(posns)
+}
+
+#This gives similarity scores
+sim_scores <- function(search_text, posns){
+  search_words <- search_text %>% tolower() %>%
+    strsplit(search_text, split = " ") %>%
+    unlist()
+  search_words <- search_words[which(search_words %in% dimnames(posns_10)$Terms)]
+  results <- colSums(posns[search_words,])
+  return(results)
+}
+
+TDM <- word_counts %>% cast_tdm(word, ID, n) %>%
+  weightSMART(spec = "btn") %>%
+  as.matrix() %>%
+  normalize() %>%
+  as.simple_triplet_matrix()
+
+SVD <- svd(TDM)
+
+#Let's say we want to find a rank-20 approximation
+
+k <- 20
+
+new_posns <- posns_k(TDM, k)
+
+some_scores <- sort(sim_scores("latent dirichlet allocation has problems", new_posns), decreasing = TRUE)[1:10] %>%
+  as_tibble(rownames = NA) %>%
+  rownames_to_column(var = "ID") %>%
+  rename(sim_score = value)
+
+full_sentences <- sentences %>%
+  filter(ID %in% some_scores$ID) %>%
+  join(some_scores) %>%
+  arrange(desc(sim_score))
---
+++ data_preparation.R
@@ -0,0 +1,42 @@
+#This R script gets the text from the markdown files in this repo and manipulates it into
+#a tidytext format from which you can do LSA, and in future hopefully other pieces of
+#analysis too
+
+library(plyr)
+library(tidytext)
+library(dplyr)
+library(stringr)
+library(SnowballC)
+
+md_files <- list.files(pattern = ".md") #get names of .md files
+
+sentences <- sapply(md_files, function(x) readLines(x)) %>% #read files
+  ldply(cbind) %>% #turn files into data frame
+  select(file = 1, text = 2) %>% #rename columns
+  filter(text != "") %>% #remove blank lines
+  filter(! str_detect(text, "#")) %>% #remove lines that are actually just section headers
+  group_by(file) %>%
+  mutate(fulltext = paste0(text, collapse = " ")) %>% #group each file's text as a big string
+  ungroup() %>%
+  select(file, fulltext) %>% #keep just file name and text
+  unique() %>%
+  unnest_tokens(sentence, fulltext, token = "sentences") %>% #break reports by sentence
+  group_by(file) %>%
+  mutate(sentence_ID = row_number()) %>% #tag sentences with IDs
+  ungroup() %>%
+  mutate(ID = paste0(file, "_", sentence_ID)) %>% 
+  select(ID, sentence) #we now have a dataframe of sentences in the repo, tagged by IDs
+
+data("stop_words")
+
+text <- sentences %>%
+  unnest_tokens(word, sentence, token = "words") %>% #break apart by words
+  filter(word != "glossary.md") %>% #remove glossary hyperlinks
+  anti_join(stop_words %>% filter(lexicon == "SMART")) %>% #remove stopwords
+  mutate(word = wordStem(word)) #stem
+
+word_counts <- text %>% count(ID, word) #count occurrences of each word in the vocab per document
+
+
+
+
